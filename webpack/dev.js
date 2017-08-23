1b9042dfc49c77c63011a96dd2d8371968502d31
Nikita Andriyanov <nikitos@itws.ru>
2017-08-23 13:01:24 +0300

    Add path to serve some static

--- webpack/dev.js
+++ webpack/dev.js
@@ -1,7 +1,11 @@
 const config = require('./common');
 const merge = require('webpack-merge');
+const path = require('path');
 
 module.exports = merge(config, {
   plugins:[
-  ]
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "../dist"),
+  }
 });
